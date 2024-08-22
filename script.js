af1a290839fa80e91e252e5fbd296df3897e1e07
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2024-08-21 21:30:26 -0400

    More styling, table looks lovely

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let pokemonData = [];
 let pokemonNames = [];
 let bool = true;
+let colors = [
+    "bg-gray-200",
+    "bg-red-200",
+    "bg-orange-200",
+    "bg-amber-200",
+    "bg-yellow-100",
+    "bg-lime-100",
+    "bg-green-200",
+    "bg-teal-400",
+    "bg-cyan-200",
+    "bg-pink-200"
+]
 async function getData(){
     try{
-        let pokemonName = document.getElementById("name").value;
+        let pokemonName = document.getElementById("name").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         
         if(!response.ok){
@@ -12,7 +24,6 @@ async function getData(){
         pokemonNames.push(pokemonName);
 
         const data = await response.json();
-        console.log(data);
         //console.log(data["abilities"])
         const numAblities = data["abilities"].length;
         const exp = data["base_experience"];
@@ -24,14 +35,22 @@ async function getData(){
         const baseAttack = data["stats"][1]["base_stat"];
         const baseDefense = data["stats"][2]["base_stat"];
         const baseSpeed = data["stats"][5]["base_stat"];
-        console.log("speed" + baseSpeed);
+        //console.log("speed" + baseSpeed);
         
-        let allData = {name:pokemonName,exp:exp,height:height,numMoves:numMoves,weight:weight,
-            species:species,baseHealth:baseHealth, baseAttack:baseAttack,
-        baseDefense:baseDefense,baseSpeed:baseSpeed}
+        let allData = {
+            name:pokemonName,
+            exp:exp,
+            height:height,
+            numMoves:numMoves,
+            weight:weight,
+            species:species,
+            baseHealth:baseHealth,
+            baseAttack:baseAttack,
+        baseDefense:baseDefense,
+        baseSpeed:baseSpeed}
 
 
-        console.log(allData.exp);
+        /*console.log(allData.exp);
         console.log(allData.height);
         console.log(allData.numMoves);
         console.log(allData.weigth);
@@ -40,7 +59,8 @@ async function getData(){
         console.log(allData.baseAttack);
         console.log(allData.baseDefense);
         console.log(allData.baseSpeed);
-
+        */
+       console.log(allData);
         pokemonData.push(allData);
         addRow(allData);
 
@@ -71,18 +91,22 @@ function addRow(object) {
 
 
 // Add a class to each cell
+    var index = 0;
     cells.forEach(cell => {
          cell.classList.add("p-3");
          cell.classList.add("text-lg");
          cell.classList.add("text-center");
          cell.classList.add("border");
-         cell.classList.add("border-gray-600");
+         cell.classList.add("border-black");
+         //cell.classList.add("border-gray-600");
+         cell.classList.add(colors[index]);
+         index+=1;
+         if (index == 10) index = 0;
         //  if (bool) {
         //     cell.classList.add("bg-gray-500");
         //  }
          //cell.classList.add("text-gray-700");
     });
-    bool = !bool;
 
     /*let newCell = newRow.insertCell(-1);
   
@@ -90,8 +114,7 @@ function addRow(object) {
     let newText = document.createTextNode(text);
     newCell.appendChild(newText);
     */
-
-
+   console.log("the index " + index)
 }
 
 
